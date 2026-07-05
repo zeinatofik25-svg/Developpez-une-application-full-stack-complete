58e4eda6b42a88b03d8c839e668f3a0c8a8fe147
AKOUM Zeina <zeina.akoum@rexel.com>
2026-07-05 22:05:03 +0200

    chore(front): align tooling and test configuration

--- front/jest.config.js
+++ front/jest.config.js
@@ -3,6 +3,22 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/app/**/*.ts',
+    '!<rootDir>/src/app/**/*.spec.ts',
+    '!<rootDir>/src/**/*.module.ts',
+    '!<rootDir>/src/main.ts',
+    '!<rootDir>/src/polyfills.ts'
+  ],
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
